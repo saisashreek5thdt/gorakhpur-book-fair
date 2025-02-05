3eac77516b4f904a6048155a672f1438ef83b68c
Sai Sashreek D <saisashreekd@gmail.com>
2025-02-05 14:14:49 +0530

    New FAQ section Created and Added

---
+++ src/Components/FAQSection.jsx
@@ -0,0 +1,240 @@
+export default function FAQSection() {
+  return (
+    <>
+      <div className="faq-area py-120">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="faq-right wow fadeInUp" data-wow-delay=".25s">
+                <div className="site-heading mb-3">
+                  <span className="site-title-tagline justify-content-start">
+                    Faq&apos;s
+                  </span>
+                  <h2 className="site-title my-3">
+                    Solutions for Your <span>Curiosities</span>
+                  </h2>
+                </div>
+                <p className="mb-3">
+                  There are many variations of passages of Lorem Ipsum
+                  available, but the majority have suffered alteration in some
+                  form, by injected humour, or randomised words which don&apos;t
+                  look even.
+                </p>
+                <p className="mb-4">
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                  accusantium doloremque laudantium, totam rem aperiam, eaque
+                  ipsa quae ab illo inventore veritatis et quasi architecto
+                  beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
+                  quia voluptas sit aspernatur aut odit aut fugit.
+                </p>
+                {/* <a href="#" className="theme-btn mt-2">
+                  Have Any Question ?
+                </a> */}
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div
+                className="accordion wow fadeInRight"
+                data-wow-delay=".25s"
+                id="accordionExample"
+              >
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingOne">
+                    <button
+                      className="accordion-button"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseOne"
+                      aria-expanded="true"
+                      aria-controls="collapseOne"
+                    >
+                      <span>
+                        <i className="far fa-question"></i>
+                      </span>{" "}
+                      Can anyone participate in festival?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseOne"
+                    className="accordion-collapse collapse show"
+                    aria-labelledby="headingOne"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div className="accordion-body">
+                      Yes, you may participate in the festival as an author,
+                      speaker, exhibitor or as a guest.
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingTwo">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseTwo"
+                      aria-expanded="false"
+                      aria-controls="collapseTwo"
+                    >
+                      <span>
+                        <i className="far fa-question"></i>
+                      </span>{" "}
+                      Can I get my book released in the festival?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseTwo"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="headingTwo"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div className="accordion-body">
+                      Yes, one can register for their book release.
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingThree">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseThree"
+                      aria-expanded="false"
+                      aria-controls="collapseThree"
+                    >
+                      <span>
+                        <i className="far fa-question"></i>
+                      </span>{" "}
+                      How to seek collaborations?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseThree"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="headingThree"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div className="accordion-body">
+                      One can write to collaborate as a sponsor.
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingFour">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseFour"
+                      aria-expanded="false"
+                      aria-controls="collapseFour"
+                    >
+                      <span>
+                        <i className="far fa-question"></i>
+                      </span>{" "}
+                      What are the internship opportunities?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseFour"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="headingFour"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div className="accordion-body">
+                      One may apply for the same at. Upon selection the interns
+                      would be notified and assigned their roles.
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingFive">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseFive"
+                      aria-expanded="false"
+                      aria-controls="collapseFive"
+                    >
+                      <span>
+                        <i className="far fa-question"></i>
+                      </span>{" "}
+                      How to reach to the venue?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseFive"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="headingFive"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div className="accordion-body">Venue Location</div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingSix">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseSix"
+                      aria-expanded="false"
+                      aria-controls="collapseSix"
+                    >
+                      <span>
+                        <i className="far fa-question"></i>
+                      </span>{" "}
+                      How can anyone volunteer in the festival?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseSix"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="headingSix"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div className="accordion-body">
+                      Volunteering is available and a training would be given to
+                      the selected volunteers for participating in the festival.
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingSeven">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseSeven"
+                      aria-expanded="false"
+                      aria-controls="collapseSeven"
+                    >
+                      <span>
+                        <i className="far fa-question"></i>
+                      </span>{" "}
+                      What is the procedure to have an allocated book stall at
+                      the festival?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseSeven"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="headingSeven"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div className="accordion-body">
+                      One may apply for the same, upon selection the book stall
+                      would be allocated.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
