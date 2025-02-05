41dd2b7a7e8c44cb7716dd1b7c688a03663d9c19
Sai Sashreek D <saisashreekd@gmail.com>
2025-02-05 14:13:41 +0530

    Updated Page Content and also added New Features Section

--- src/Components/EventRegister.jsx
+++ src/Components/EventRegister.jsx
@@ -1,5 +1,6 @@
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import eventBG from "../assets/img/cta/01.jpg";
+import EventFeatures from "./EventFeatures";
 
 export default function EventRegister() {
   const eventRegisterBG = {
@@ -14,19 +15,22 @@ export default function EventRegister() {
           <div className="row">
             <div className="col-lg-8 mx-auto">
               <div className="cta-content wow fadeInUp" data-wow-delay=".25s">
-                <h1>Are you want to join our events ?</h1>
+                <h1>Get Your Registration?</h1>
                 <p>
                   It is a long established fact that a reader will be distracted
                   by the readable <br />
                   content of a page when looking at its layout.
                 </p>
-                <Link to="/" className="theme-btn">
+                {/* <Link to="/" className="theme-btn">
                   Register Now<i className="fas fa-arrow-right"></i>
-                </Link>
+                </Link> */}
               </div>
-            </div>
+            </div>            
           </div>
         </div>
+      </div>
+      <div className="py-3">
+        <EventFeatures />
       </div>
     </>
   );
