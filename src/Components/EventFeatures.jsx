8f93e422c1dfe72c17461be641c6d9d8d60d5341
Sai Sashreek D <saisashreekd@gmail.com>
2025-02-07 15:28:30 +0530

    Added custom json and swiper classes using Swiper Package

--- src/Components/EventFeatures.jsx
+++ src/Components/EventFeatures.jsx
@@ -1,149 +1,102 @@
 import { Link } from "react-router-dom";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
 import icon1 from "../assets/img/icon/speaker.svg";
 import icon2 from "../assets/img/icon/learn.svg";
 import icon3 from "../assets/img/icon/meet.svg";
 import icon4 from "../assets/img/icon/question.svg";
 
 export default function EventFeatures() {
+  const eventHighlights = [
+    {
+      id: 1,
+      imgURL: icon1,
+      title: "Vendor",
+      desc: "It is a long established fact that a reader will be distracted.",
+    },
+    {
+      id: 2,
+      imgURL: icon2,
+      title: "Volunteer",
+      desc: "It is a long established fact that a reader will be distracted.",
+    },
+    {
+      id: 3,
+      imgURL: icon3,
+      title: "Exhibitor",
+      desc: "It is a long established fact that a reader will be distracted.",
+    },
+    {
+      id: 4,
+      imgURL: icon4,
+      title: "Visitor",
+      desc: "It is a long established fact that a reader will be distracted.",
+    },
+    {
+      id: 5,
+      imgURL: icon1,
+      title: "Speaker",
+      desc: "It is a long established fact that a reader will be distracted.",
+    },
+    {
+      id: 6,
+      imgURL: icon3,
+      title: "Author",
+      desc: "It is a long established fact that a reader will be distracted.",
+    },
+    {
+      id: 7,
+      imgURL: icon2,
+      title: "Sponsoror",
+      desc: "It is a long established fact that a reader will be distracted.",
+    },
+  ];
+
   return (
     <>
       <div className="feature-area fa-negative">
         <div className="container">
           <div className="feature-wrapper">
             <div className="row g-4">
-              <div className="col-md-6 col-lg-3">
-                <div
-                  className="feature-item wow fadeInUp"
-                  data-wow-delay=".25s"
-                >
-                  <span className="count">01</span>
-                  <div className="feature-icon">
-                    <img src={icon1} alt="" />
-                  </div>
-                  <h4 className="feature-title">Vendor</h4>
-                  <p>
-                    It is a long established fact that a reader will be
-                    distracted.
-                  </p>
-                  <Link to="/" className="theme-btn mt-20">
-                    Register<i className="fas fa-arrow-right"></i>
-                  </Link>
-                </div>
-              </div>
-              <div className="col-md-6 col-lg-3">
-                <div
-                  className="feature-item wow fadeInDown"
-                  data-wow-delay=".25s"
-                >
-                  <span className="count">02</span>
-                  <div className="feature-icon">
-                    <img src={icon2} alt="" />
-                  </div>
-                  <h4 className="feature-title">Volunteer</h4>
-                  <p>
-                    It is a long established fact that a reader will be
-                    distracted.
-                  </p>
-                  <Link to="/" className="theme-btn mt-20">
-                    Register<i className="fas fa-arrow-right"></i>
-                  </Link>
-                </div>
-              </div>
-              <div className="col-md-6 col-lg-3">
-                <div
-                  className="feature-item wow fadeInUp"
-                  data-wow-delay=".25s"
-                >
-                  <span className="count">03</span>
-                  <div className="feature-icon">
-                    <img src={icon3} alt="" />
-                  </div>
-                  <h4 className="feature-title">Exhibitor</h4>
-                  <p>
-                    It is a long established fact that a reader will be
-                    distracted.
-                  </p>
-                  <Link to="/" className="theme-btn mt-20">
-                    Register<i className="fas fa-arrow-right"></i>
-                  </Link>
-                </div>
-              </div>
-              <div className="col-md-6 col-lg-3">
-                <div
-                  className="feature-item wow fadeInDown"
-                  data-wow-delay=".25s"
-                >
-                  <span className="count">04</span>
-                  <div className="feature-icon">
-                    <img src={icon4} alt="" />
-                  </div>
-                  <h4 className="feature-title">Visitor</h4>
-                  <p>
-                    It is a long established fact that a reader will be
-                    distracted.
-                  </p>
-                  <Link to="/" className="theme-btn mt-20">
-                    Register<i className="fas fa-arrow-right"></i>
-                  </Link>
-                </div>
-              </div>
-              <div className="col-md-6 col-lg-3">
-                <div
-                  className="feature-item wow fadeInUp"
-                  data-wow-delay=".25s"
-                >
-                  <span className="count">05</span>
-                  <div className="feature-icon">
-                    <img src={icon1} alt="" />
-                  </div>
-                  <h4 className="feature-title">Speaker</h4>
-                  <p>
-                    It is a long established fact that a reader will be
-                    distracted.
-                  </p>
-                  <Link to="/" className="theme-btn mt-20">
-                    Register<i className="fas fa-arrow-right"></i>
-                  </Link>
-                </div>
-              </div>
-              <div className="col-md-6 col-lg-3">
-                <div
-                  className="feature-item wow fadeInUp"
-                  data-wow-delay=".25s"
-                >
-                  <span className="count">06</span>
-                  <div className="feature-icon">
-                    <img src={icon3} alt="" />
-                  </div>
-                  <h4 className="feature-title">Author</h4>
-                  <p>
-                    It is a long established fact that a reader will be
-                    distracted.
-                  </p>
-                  <Link to="/" className="theme-btn mt-20">
-                    Register<i className="fas fa-arrow-right"></i>
-                  </Link>
-                </div>
-              </div>
-              <div className="col-md-6 col-lg-3">
-                <div
-                  className="feature-item wow fadeInUp"
-                  data-wow-delay=".25s"
-                >
-                  <span className="count">07</span>
-                  <div className="feature-icon">
-                    <img src={icon2} alt="" />
-                  </div>
-                  <h4 className="feature-title">Sponsor</h4>
-                  <p>
-                    It is a long established fact that a reader will be
-                    distracted.
-                  </p>
-                  <Link to="/" className="theme-btn mt-20">
-                    Register<i className="fas fa-arrow-right"></i>
-                  </Link>
-                </div>
-              </div>
+              <Swiper                
+                className="swiper"
+                slidesPerView={3}
+                spaceBetween={100}
+                loop={true}
+                autoplay={{
+                  delay: 3000, // Adjust delay time (3 seconds here)
+                  disableOnInteraction: false,
+                }}
+                modules={[Autoplay, Pagination]}
+                breakpoints={{
+                  320: { slidesPerView: 1 }, // Mobile view shows 1 slide
+                  768: { slidesPerView: 2 }, // Tablet view shows 2 slides
+                  1024: { slidesPerView: 3 }, // Desktop view shows 3 slides
+                }}
+              >
+                {eventHighlights.map((event, index) => (
+                  <SwiperSlide key={index}>
+                    <div className="col-md-6 col-lg-3" style={{ width: "275px" }}>
+                      <div
+                        className="feature-item wow fadeInDown"
+                        data-wow-delay=".25s"
+                      >
+                        <span className="count"></span>
+                        <div className="feature-icon">
+                          <img src={event.imgURL} alt="" />
+                        </div>
+                        <h4 className="feature-title">{event.title}</h4>
+                        <p>{event.desc}</p>
+                        <Link to="/" className="theme-btn mt-20">
+                          Register<i className="fas fa-arrow-right"></i>
+                        </Link>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
             </div>
           </div>
         </div>
